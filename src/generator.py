c59ba66fdb519171fc65be98a44eae0f575cbf05
xixuangezu <xixuangezu@163.com>
2018-12-11 18:44:33 +0800

    csv解析

--- src/generator.py
+++ src/generator.py
@@ -3,6 +3,29 @@
 
 from setting import *
 import chardet
+import csv
+
+class IOEncode:
+    """
+    处理io编码问题
+    """
+    @staticmethod
+    def fileEncoding(filePoint):
+        """
+        解析文件所用编码
+
+        Args:
+            filePoint: 文件指针
+        """
+        fpP = filePoint.tell()
+        allLine = filePoint.read()
+        filePoint.seek(fpP)
+
+        encoding = chardet.detect(allLine)["encoding"]
+        if not encoding:
+            print "warning: IOEncode.fileEncoding() 无法自动检测编码  默认为: ", "GB2312", " 文件: ", filePoint.name
+        
+        return encoding
 
 class CsvMaker:
     """
@@ -10,34 +33,43 @@ class CsvMaker:
     """
 
     @staticmethod
-    def makeOneLine(filePoint):
+    def makeDict(filePoint, arrKeys = [], encoding = None):
         """
-        解析单行  默认解析 文件 当前指针行
+        解析csv为字典  并转换编码
 
         Args:
             filePoint: csv文件指针
+            arrKeys: 字典key组成的数组
+            encoding: 转换成编码 默认
         """
-        fp = open("../test/UIConfig.csv", "r")
-        allLine = fp.read()
-        fp.seek(0)
-        oneLine = fp.readline()
-        fp.close()
-        encoding = chardet.detect(allLine)["encoding"]
-        ttencoding = chardet.detect("warning: 无法自动检测编码  默认为: ")["encoding"]
-        print ttencoding
-        if not encoding:
-            print "warning: 无法自动检测编码  默认为: ", "GB2312"
-        else:
-            print "自动检测编码  结果为: ", encoding
-        utf8OneLine = (oneLine.decode(encoding)).encode("utf-8")
+        encoding = encoding or "utf-8"
 
-        print "一行", utf8OneLine
+        fileEncoding = IOEncode.fileEncoding(filePoint)
 
-import sys
+        csvReader = csv.reader(filePoint, delimiter=',', quotechar='"')
 
-print sys.stdout.encoding
+        csvData = [[data.decode(fileEncoding).encode(encoding) 
+                    for data in line]
+                    for line in csvReader]
+
+        if not (csvData[0] and csvData[0][0]):
+            print "error: CsvMaker.makeDict() file: \"" + filePoint.name + "\" 没有内容"
+            return [{}]
+
+        #平齐key与var的数量
+        keys = []
+        for i in range(len(csvData[0])):
+            if len(arrKeys) > i:
+                keys.insert(i, arrKeys[i])
+            else:
+                keys.insert(i, "key" + str(i))
+
+        csvDict = [dict(zip(keys, line)) for line in csvData]
+
+        return csvDict
 
 def main():
-    CsvMaker.makeOneLine(" ")
+    with open("../test/UIConfig.csv", "r") as fp:
+        print CsvMaker.makeDict(fp, ["tt0", "tt1", "tt2"], "utf-8")[0]["tt0"]
 
 main()
--- src/setting.py
+++ src/setting.py
@@ -5,17 +5,17 @@
 import sys
 import locale
   
-class UnicodeStreamFilter:  
-    def __init__(self, target):  
-        self.target = target  
-        self.encoding = 'utf-8'  
-        self.errors = 'replace'  
+class UnicodeStreamFilter:
+    def __init__(self, target):
+        self.target = target
+        self.encoding = 'utf-8'
+        self.errors = 'replace'
         self.encode_to = locale.getpreferredencoding()
-    def write(self, s):  
-        if type(s) == str:  
-            s = s.decode("utf-8")  
+    def write(self, s):
+        if type(s) == str:
+            s = s.decode("utf-8")
         s = s.encode(self.encode_to, self.errors)
-        self.target.write(s)  
+        self.target.write(s)
           
 # if locale.getpreferredencoding() == 'cp936':
 sys.stdout = UnicodeStreamFilter(sys.stdout)
